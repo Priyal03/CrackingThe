5e000c02f8a6183e7d2739d23402a3450e80e13a
PriyalPatel <priyalpatel94@gmail.com>
2022-02-26 01:17:11 -0600

    Time: 0 ms (100.00%), Space: 44.6 MB (30.60%) - LeetHub

--- 1120-maximum-average-subtree/1120-maximum-average-subtree.java
+++ 1120-maximum-average-subtree/1120-maximum-average-subtree.java
@@ -1,58 +1,38 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    // for each node in the tree, we will maintain three values
-    class State {
-        // count of nodes in the subtree
-        int nodeCount;
-
-        // sum of values in the subtree
-        int valueSum;
-
-        // max average found in the subtree
-        double maxAverage;
-
-        State(int nodes, int sum, double maxAverage) {
-            this.nodeCount = nodes;
-            this.valueSum = sum;
-            this.maxAverage = maxAverage;
-        }
-    }
-
+    private double max = 0;
     public double maximumAverageSubtree(TreeNode root) {
-        return maxAverage(root).maxAverage;
-    }
-
-    State maxAverage(TreeNode root) {
         if (root == null) {
-            return new State(0, 0, 0);
+            return 0;
         }
-
-        // postorder traversal, solve for both child nodes first.
-        State left = maxAverage(root.left);
-        State right = maxAverage(root.right);
-
-        // now find nodeCount, valueSum and maxAverage for current node `root`
-        int nodeCount = left.nodeCount + right.nodeCount + 1;
-        int sum = left.valueSum + right.valueSum + root.val;
-        double maxAverage = Math.max(
-                (1.0 * (sum)) / nodeCount, // average for current node
-                Math.max(right.maxAverage, left.maxAverage) // max average from child nodes
-        );
-
-        return new State(nodeCount, sum, maxAverage);
+        
+        traverse(root);
+        return max;
+    }
+    
+    private int traverse(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        
+        int left = traverse(root.left);
+        int right = traverse(root.right);
+        int curr = 0;
+        int currSum = 0;
+        if (left != -1) {
+            curr++;
+            currSum += left;
+        }
+        
+        if (right != -1) {
+            curr++;
+            currSum += right;
+        }
+        
+        double value = (currSum + root.val)/ (double)(curr + 1);
+       // System.out.println(value);
+        max = Math.max(value, max);
+        
+        return (int)value;
+        
     }
 }
