3719f503197a6265cb7c9bb1e3c22336b5ed3a37
PriyalPatel <priyalpatel94@gmail.com>
2022-02-25 23:29:06 -0600

    Time: 85 ms (16.67%), Space: 101.7 MB (5.02%) - LeetHub

--- 1648-sell-diminishing-valued-colored-balls/1648-sell-diminishing-valued-colored-balls.java
+++ 1648-sell-diminishing-valued-colored-balls/1648-sell-diminishing-valued-colored-balls.java
@@ -1,35 +1,46 @@
 class Solution {
+    private long mod = 1000000007L;
     public int maxProfit(int[] inventory, int orders) {
+		// we use pq to find the most balls
+        PriorityQueue<Long> pq = new PriorityQueue<>((x, y) -> Long.compare(y, x));
+        pq.offer(0L);
+		
+        // we use map to count the balls
+        Map<Long, Long> map = new HashMap<>();
+        map.put(0L, 0L);
         
-        int n = inventory.length;
-        long MOD = (long)1e9 + 7;
-        long totalValue = 0;
-        
-        Arrays.sort(inventory);
-        
-        int count = 0;
-        for (int i = n - 1; i >= 0; i--){
-            count++;
-            if (i == 0 || inventory[i] > inventory[i - 1]){
-                long diff = i == 0 ? inventory[i] : inventory[i] - inventory[i - 1];
-                if (count * diff < orders) {
-                    totalValue += 
-                        (2 * inventory[i] - diff + 1)
-                        * diff * count / 2 % MOD;
-                    orders -= count * diff;
-                } else {
-                    diff = orders / count;
-                    long remainder = orders % count;
-                                    
-                    totalValue += 
-                        (2 * inventory[i] - diff + 1)
-                        * diff * count / 2 % MOD;
-                    totalValue += (inventory[i] - diff) * remainder % MOD;
-                    totalValue %= MOD;
-                    break;
-                }
+        for (int j : inventory) {
+            long i = (long)j;
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            }
+            else {
+                pq.offer(i);
+                map.put(i, 1L);
             }
         }
-        return (int)totalValue;
+        
+        long res = 0;
+        while (orders > 0) {
+            long ball = pq.poll(), nextBall = pq.peek();
+            long times = map.get(ball);
+            long diff = Math.min(ball - nextBall, orders / times);
+            if (diff == 0) {
+                res = (res + orders * ball) % mod;
+                break;
+            }
+            long sum = (ball * 2 + 1 - diff) * diff / 2 * times;
+            res = (res + sum) % mod;
+            orders -= diff * times;
+            if (!map.containsKey(ball - diff)) {
+                map.put(ball - diff, map.get(ball));
+                pq.offer(ball - diff);
+            }
+            else {
+                map.put(ball - diff, map.get(ball - diff) + map.get(ball));
+            }
+            map.remove(ball);
+        }
+        return (int) res;
     }
 }
